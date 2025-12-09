fdb6ab17e2ea8bfcabf45a4e32c162ce25e23524
Parvesh <arveshchaudhary@gmail.com>
2025-12-09 19:01:50 +0545

    fix bug

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Components/Navbar'
-import Footer from './components/Footer'
+import Footer from './Components/Footer'
 
 const Layout = () => {
   return (
